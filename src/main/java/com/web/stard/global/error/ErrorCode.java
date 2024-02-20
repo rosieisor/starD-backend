3d86a2b9f8f8a71f714fb8a8aa664068a4dd9870
강지륜 <kjr2699@naver.com>
2024-02-20 15:12:07 +0900

    feat: error exception 추가

---
+++ src/main/java/com/web/stard/global/error/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.web.stard.global.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원"),
+    INVALID_TOKEN(HttpStatus.FORBIDDEN, "토큰 검증 실패");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
