49fe580362d2e572c0b8448ee0cbf406209f51f3
강지륜 <kjr2699@naver.com>
2024-10-04 13:05:10 +0900

    test:깃연동

--- src/main/java/com/web/stard/global/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/web/stard/global/handler/CustomAuthenticationFailureHandler.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
+    /*
+    * */
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
